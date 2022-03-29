05629b6f08473701f6855c1076fef85c712347fc
Rice Shelley <jrs0100@uah.edu>
2022-03-28 22:46:49 -0500

    added MDIO stuff fixed RMII bugs

--- sim/mdio_ctrl/mdio_sim.py
+++ sim/mdio_ctrl/mdio_sim.py
@@ -4,57 +4,55 @@ import string
 import random
 from cocotb.clock import Clock
 from cocotb.triggers import Timer, RisingEdge
-from cocotb.utils import get_sim_time
 
 # Test MDIO read
 @cocotb.test()
 async def mdio_rd_test(dut):
     dut.start.value = 0
     dut.wr.value = 0
-    dut.phy_addr.value = 1
+    dut.phy_addr.value = 5
     dut.reg_addr.value = 3
     dut.data_in.value = 0
 
-    clock = Clock(dut.clk, 10, units="ns")
+    clock = Clock(dut.mdio_clk, 10, units="ns")
     cocotb.start_soon(clock.start())
 
-    await RisingEdge(dut.clk)
+    await RisingEdge(dut.mdio_clk)
     dut.start.value = 1
-    await RisingEdge(dut.clk)
+    await RisingEdge(dut.mdio_clk)
     dut.start.value = 0
-    for _ in range(64 - 16):
-        await RisingEdge(dut.mdio_mdc)
+    for _ in range(0, 64 - 16):
+        await RisingEdge(dut.mdio_clk)
 
-    data = 0xBFA5
-    for i in range(16):
-        dut.mdio_data_in.value = (data & 0x8000) >> 15
+    data = 0x3FA5
+    for _ in range(0, 16):
+        await RisingEdge(dut.mdio_clk)
+        dut.mdio_data.value = (data & 0x8000) >> 15
         data = data << 1
-        await RisingEdge(dut.mdio_mdc)
-        print(str(i) + " time " + str(get_sim_time()))
-    
-    for _ in range(0, 128):
-        await RisingEdge(dut.clk)
 
-    await Timer(10, "us")
+    for _ in range(0, 128):
+        await RisingEdge(dut.mdio_clk)
+
+    await Timer(100, "ns")
 
 # Test MDIO write
 @cocotb.test()
 async def mdio_wr_test(dut):
     dut.start.value = 0
     dut.wr.value = 1
-    dut.phy_addr.value = 1
+    dut.phy_addr.value = 5
     dut.reg_addr.value = 3
     dut.data_in.value = 0xF0CA
 
-    clock = Clock(dut.clk, 10, units="ns")
+    clock = Clock(dut.mdio_clk, 10, units="ns")
     cocotb.start_soon(clock.start())
 
-    await RisingEdge(dut.clk)
+    await RisingEdge(dut.mdio_clk)
     dut.start.value = 1
-    await RisingEdge(dut.clk)
+    await RisingEdge(dut.mdio_clk)
     dut.start.value = 0
 
-    for _ in range(64):
-        await RisingEdge(dut.mdio_mdc)
+    for _ in range(0, 128):
+        await RisingEdge(dut.mdio_clk)
 
-    await Timer(10, "us")
+    await Timer(100, "ns")
